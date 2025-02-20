942961305e3ec5d776ff77059ccb16a6a9d17184
Sb-CCL <sbcolorcodedlabs@gmail.com>
2025-02-19 20:03:45 -0500

    updated the axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import Search from "./components/Search";
 
 function App() {
-  return <>Main App</>;
+  return (
+    <>
+      <Search />
+    </>
+  );
 }
 
 export default App;
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,7 +1,56 @@
-import React from "react";
+import { useState } from "react";
+import axios from "axios";
 
 const Search = () => {
-  return <div>Search</div>;
+  const [name, setName] = useState("");
+  const [country, setCountry] = useState("");
+  const [universities, setUniversities] = useState([]);
+
+  const fetchUniversities = async () => {
+    try {
+      const { data } = await axios.get(
+        `http://universities.hipolabs.com/search?name=${name}&country=${country}`
+      );
+      setUniversities(data);
+    } catch {
+      alert("Error fetching university data");
+    }
+  };
+
+  return (
+    <div>
+      <h1>University Search</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </div>
+      <div>
+        <input
+          type="text"
+          placeholder="Country"
+          value={country}
+          onChange={(e) => setCountry(e.target.value)}
+        />
+      </div>
+      <button onClick={fetchUniversities}>Search</button>
+      <div>
+        <ul>
+          {universities.map((university) => (
+            <li key={university.name}>
+              <p>{university.name}</p>
+              <a href={university.web_pages[0]} target="_blank">
+                {university.web_pages[0]}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Search;
